c30407d9c5e123a272cbd59b9624eeb4211c49fe
Wil Cook <wil.cook.is@gmail.com>
2018-11-26 20:32:01 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,79 @@
-# Write your code here.
+require "pry"
+
+dictionary = {
+    "hello" => "hi", 
+    "to" => "2", 
+    "two" => "2", 
+    "too" => "2", 
+    "for" => "4", 
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
+    
+def word_substituter(tweet)
+  
+dictionary = {
+    "hello" => "hi", 
+    "to" => "2", 
+    "two" => "2", 
+    "too" => "2", 
+    "for" => "4",
+    "For" => "4",
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
+   
+  tweet_array = tweet.split(" ")
+  # binding.pry
+  changed_array = []
+  tweet_array.map do |word|
+    # binding.pry
+    dictionary.map do |key, value|
+      if word == key
+        word = value
+        # binding.pry
+      end
+    end
+    changed_array << word
+  end
+  changed_array.join(" ")
+  # binding.pry
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+
+end
+
+def selective_tweet_shortener(tweet)
+  # binding.pry
+  if tweet.length > 140
+    puts word_substituter(tweet)
+  else 
+    puts tweet
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
